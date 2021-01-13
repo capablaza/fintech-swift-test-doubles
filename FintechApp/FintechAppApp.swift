f0c79a04c831aba96eb9c857a99e5adbb053cf36
carlos <c.apablaza.z@gmail.com>
2021-01-13 09:38:28 -0300

    Initial Commit

---
+++ FintechApp/FintechAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FintechAppApp.swift
+//  FintechApp
+//
+//  Created by carlos on 13-01-21.
+//
+
+import SwiftUI
+
+@main
+struct FintechAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
